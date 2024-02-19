198877e7e397326852e156c80e5cc35048a11ab4
X1mpleR <118037245+X1mpleR@users.noreply.github.com>
2024-02-19 18:13:42 +0800

    9

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	 sus = append(sus, 853760146)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 853760146)
+	sus = append(sus, 853760146)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
